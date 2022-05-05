5bc12c85598d9f2d402b6cf4b6c7dc10fc386e9e
cecialbero <ceci.albero@gmail.com>
2022-05-05 19:09:51 -0300

    Include React Query on PokeDetails

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import PokesDek from './pages/PokesDek'
 import PokeDetails from './pages/PokeDetails'
 import NoMatch from './pages/NoMatch'
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path="/" element={<PokesDek />} />
         <Route path='/poke/:id' element={<PokeDetails />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/PokeDetails.js
+++ src/pages/PokeDetails.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
+import { useQuery } from 'react-query'
 import Chart from '../components/Chart'
 import Loader from '../components/Loader'
 import { Board, BoardFigure } from '../styled-components/Board'
@@ -10,66 +10,49 @@ import { Alert } from '../styled-components/Alert'
 const PokeDetails = () => {
   const { id } = useParams()
   const navigate = useNavigate()
-  const [poke, setPoke] = useState({})
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
 
-  const getPoke = async id => {
-    try {
-      setLoading(true)
-      let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      let data = await res.json()
+  const { isLoading, error, data } = useQuery('pokeApi', () => 
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(res => 
+      res.json()
+    )
+  )
 
-      setPoke(data)
-    } catch(err) {
-      setError(err)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    getPoke(id)
-  }, [id])
-
-  const handleReturn = () => {
-      navigate( -1 );
+  if(isLoading) {
+    return <Loader />
   }
 
   if(error) {
     return <Alert type={'error'}>Ups, looks like we couldn't catch any Pokemon. Try later</Alert>
   }
 
-  const { name, sprites, stats } = poke
+  const handleReturn = () => {
+      navigate( -1 );
+  }
+
+  const { name, stats, sprites } = data
 
   return (
     <Container mxWidth={'1000px'}>
-      {
-        loading
-        ? <Loader />
-        : <>
-            <Board>
-              <Row>
-                <Column size={1}>
-                  <h2>{name}</h2>
-                </Column>
-              </Row>
-              <Row centered>
-                <Column size={1}>
-                  <BoardFigure>
-                    <img src={sprites.other.dream_world.front_default} alt={name}/>
-                  </BoardFigure>
-                </Column>
-                <Column size={1}>
-                  {stats.map(el => <Chart key={el.stat.name} value={el.base_stat} label={el.stat.name.replace(/-/, ' ')} />)}
-                </Column>
-              </Row>
-            </Board>
-            <Button className='mt-30' onClick={ handleReturn }>
-              Back to Dek 
-            </Button>
-          </>
-      }
+      <Board>
+        <Row>
+          <Column size={1}>
+            <h2>{name}</h2>
+          </Column>
+        </Row>
+        <Row centered>
+          <Column size={1}>
+            <BoardFigure>
+              <img src={sprites.other.dream_world.front_default} alt={name}/>
+            </BoardFigure>
+          </Column>
+          <Column size={1}>
+            {stats.map(el => <Chart key={el.stat.name} value={el.base_stat} label={el.stat.name.replace(/-/, ' ')} />)}
+          </Column>
+        </Row>
+      </Board>
+      <Button className='mt-30' onClick={ handleReturn }>
+        Back to Dek 
+      </Button>
     </Container>
   )
 }
