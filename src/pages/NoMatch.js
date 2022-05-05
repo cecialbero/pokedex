4ac03c6240ffc0df1678d5d2c85d2cff862cf88e
cecialbero <ceci.albero@gmail.com>
2022-05-05 18:17:38 -0300

    Style the no match page

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Container } from '../styled-components/Grid'
+import { Board } from '../styled-components/Board'
+import { Button } from '../styled-components/Button'
+import { useNavigate } from 'react-router-dom'
 
 const NoMatch = () => {
+  const navigate = useNavigate()
+
+  const goToDek = () => {
+    navigate('/')
+  }
+
   return (
-    <div>NoMatch</div>
+    <Container mxWidth={'1000px'} className='no-match'>
+      <Board>
+        <img src='/img/pokeball.gif' alt='moving pokeball' width='150'/>
+        <h2>No Pokemons over here</h2>
+        <p>To find one, go <Button onClick={ goToDek }>back to the dek</Button></p>
+      </Board>
+    </Container>
   )
 }
 
